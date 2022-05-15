b59103b4568c7afa7edd9db5f3f556e41e0fdbc4
Kirill <pimenov.k@inbix.ru>
2022-05-15 19:33:32 +0300

    add loader on album

--- src/components/baseTimer.vue
+++ src/components/baseTimer.vue
@@ -37,7 +37,7 @@ export default {
     },
     complete() {
       document.getElementById("timer").style.display = "none";
-      //this.$root.socket.emit("timeIsUp");
+      this.$root.socket.emit("timeIsUp");
     },
   },
   computed: {
--- src/views/History.vue
+++ src/views/History.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="lobby">
+  <Loader id="load"></Loader>
+  <div class="history">
     <router-link class="back" to="/"> home </router-link>
     <div class="room">
       <section class="players">
@@ -16,7 +17,9 @@
             <p class="end-p">end of {{ name }}'s album</p>
           </div>
 
-          <button class="write">new turn</button>
+          <router-link class="write" to="#" @click="newGame"
+            >Играть ещё</router-link
+          >
         </div>
       </section>
     </div>
@@ -25,11 +28,14 @@
 
 <script >
 import { mapGetters } from "vuex";
-
+import Loader from "../components/loader.vue";
 export default {
+  components: {
+    Loader,
+  },
   data() {
     return {
-      name: "",
+      name: (String = "Гость123123123"),
     };
   },
   mounted() {
@@ -37,7 +43,42 @@ export default {
       this.name = localStorage.name;
     }
   },
-  methods: {},
+  methods: {
+    async newGame() {
+      let loader = document.getElementById("load");
+
+      let body = document.getElementsByClassName("history")[0];
+
+      let content = document.getElementById("content");
+      content.style.border = "none";
+
+      body.style.display = "none";
+
+      loader.style.display = "block";
+
+      let url = "http://localhost:3000/login/" + this.name;
+      let resp = "notok";
+
+      await axios.get(url).then((response) => {
+        resp = response.data.result;
+        loader.style.display = "none";
+        body.style.display = "flex";
+        content.style.border = "4px #f5d731f1 solid";
+      });
+
+      if (resp != "ok") {
+        alert(
+          "В настоящий момент имя " +
+            this.name +
+            " уже используется, придумайте другое!"
+        );
+        return;
+      }
+      localStorage.name = this.name;
+
+      this.$router.push("/lobby");
+    },
+  },
   computed: {
     ...mapGetters(["getNumPlayers", "getPlayers"]),
   },
@@ -45,24 +86,34 @@ export default {
 </script>
 
 <style>
+#load {
+  display: none;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+}
+.room {
+  justify-content: space-between;
+  padding: 0 10px;
+}
 .album {
   border-radius: 20px;
   background: rgba(255, 234, 13, 0.28);
   min-width: 55%;
-  max-width: 65%;
   display: inherit;
   flex-direction: column;
   align-items: center;
   font-size: 2.5vw;
   padding: 0 0.5em;
 }
-
-.end {
+.end-game {
   align-self: stretch;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
-  margin: 20px;
+  margin: 15px;
+}
+.end {
 }
 
 .end-p {
@@ -73,7 +124,7 @@ export default {
   text-align: center;
   text-transform: uppercase;
   line-height: 18px;
-  margin: 0px 10px;
+  margin: 0px auto;
 }
 
 .end-p::before,
